6e578b27e388fd84b8ce6bc732b601a3801f34a3
harshalpatil655 <harshalpatil655@gmail.com>
2022-12-23 17:01:28 +0530

    aadedassignments

---
+++ JavaScriptPract/index.js
@@ -0,0 +1,107 @@
+///Hoisting
+// let b = 20;
+// function add() {
+//   console.log(b);
+// }
+// add();
+// var a = 10;
+
+// const c = 30;
+
+///Call back & call back hell
+
+// setTimeout(() => {});
+
+///Scopes & lexical environment
+
+// function one() {
+//   let a = 10;
+//   function two() {
+//     let b = 20;
+//     return a + b;
+//   }
+//   two();
+// }
+// one();
+
+///primitive & non primitive
+
+// let a = "harshal";
+
+// let b = a; //pointing to new memory location
+
+// b = "amit";
+// console.log(a);          ///shallow copy
+// console.log(b);
+// let x = { name: "harshal" };
+
+// let y = x; //points to same address
+// y.name = "amit";
+// console.log(x);    ///deep copy
+// console.log(y);
+
+// first-class function
+
+// let x = function add(a, b) {
+//   a + b;                                 //when func is stored inside variable
+// };
+
+///Anonmous and non anonmous function
+
+// let x = function () {}; //function which doesn't have any name
+
+///IIFE Immediately invoke function
+
+// (function add(a, b) {
+//   console.log(a + b);
+// })(5, 4);
+
+/////////  Closures
+
+// function one() {
+//   let a = 10;
+//   let m = function two() {
+//     let b = 10;
+//     return a + b;
+//   };
+//   console.log(m());
+// }
+// one();
+
+////Higher order function
+
+///map func
+// returns array
+// let arr = [1, 2, 3, 4, 5];
+
+// output = [2, 4, 6, 8, 10];
+
+///forEach
+// let arr = [1, 2, 3, 4, 5];
+//doesnt return anything
+// arr.forEach((element) => {
+//   if (element % 2 === 0) {
+//     console.log(element);
+//   }
+// });
+
+////filter
+
+// let arr = [1, 2, 3, 4, 5];
+
+// let x = arr.filter((element) => {
+//   if (element % 2 === 0) {
+//     return element;
+//   }
+// });
+// console.log(x);
+
+/////////////////Reduce /////////////////////////
+
+let arr = [1, 2, 3, 4, 5];
+
+let x = arr.reduce((acc, curr) => {
+  acc = acc + curr;
+  return acc;
+}, 0);
+console.log(x);
---
+++ Practice/index.js
@@ -0,0 +1,36 @@
+// console.log(a);
+// var a = 10;
+// console.log(x);
+// let x = function demo() {
+//   console.log("Masai");
+// };
+
+// if(){
+// console.log("Harshal")
+// }
+
+// let x = function () {
+//   console.log("Masai");
+// };
+
+// (function add(a, b) {
+//   console.log(a + b);
+// })(5, 4);
+// iife immediate invoke func
+
+// function x(a) {
+//   function y(b) {
+//     return a + b;
+//   }
+//   return y(3);
+// }
+// console.log(x(5));
+
+// let arr = [1, 2, 3, 4, 5, 6];
+
+// let z = arr.forEach((element) => {
+//   if (element % 2 == 0) {
+//     return element;
+//   }
+// });
+// console.log(z);
